8fb66fc6865c5e81d08499f86aa1db7b847c37e6
kimwansoo8947 <ch3558947@naver.com>
2025-03-24 10:51:02 +0900

    three

--- week03.py
+++ week03.py
@@ -1,6 +1,7 @@
 # 3주차 배열
+import array
 
-array = [11,9,-77,8] # 파이썬은 배열 개념이 없음
+arr = array.array ('f' ,[11,9,-77,8]) # 파이썬은 배열 개념이 없음
 
-for i in range(len(array)):
-    print(f"{array[i]:3}{id(array[i])}")
+for i in range(len(arr)):
+    print(f"{arr[i]:3}{id(arr[i])}")
