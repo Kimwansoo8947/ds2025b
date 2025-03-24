23b32ee3715f4d37904117c5def23b7b3cdcc570
kimwansoo8947 <ch3558947@naver.com>
2025-03-24 11:42:04 +0900

    three_02

--- week03.py
+++ week03.py
@@ -1,26 +1,6 @@
-def move_zeros(a_list):
-    zero_index = 0
+groups = ['HOT','Seventeen', 'Black Pink',"NJZ"]
+#ratings = [1,2,4,3,100]
+ratings = [1,2,4,3]
 
-    for index,  n in enumerate(a_list):
-        if n != 0:
-            a_list[zero_index] = n
-            if zero_index != index:
-                a_list[index] = 0
-            zero_index += 1
-
-    return (a_list)
-a_list = [8,0,3,0,12]
-move_zeros(a_list)
-print(a_list)
-
-
-
-# for i,v in enumerate(l): # unpacking
-   # print(i,v)
-# for i in enumerate(l): # packing
-   #  print(i) # 인덱스랑 값을 동시에 리턴 (튜플)
-
-
-# l = [11,9,-77,8]
-# for i in range(len(l)):
-    # print(i, l[i])
+groups_rating = list(zip(groups, ratings)) # zip 함수
+print(groups_rating)
