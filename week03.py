3825b75fa13c72b0da95c908bd4067114fac943c
kimwansoo8947 <ch3558947@naver.com>
2025-03-24 11:55:21 +0900

    0.9 array

--- week03.py
+++ week03.py
@@ -5,7 +5,27 @@
 # groups_rating = list(zip(groups, ratings)) # zip 함수
 # print(groups_rating)
 
-city =['Incheon','Incheon','Incheon','Gimpo','Seoul','Seoul']
-# city = {'Incheon','Incheon','Incheon','Gimpo','Seoul','Seoul'}88
-city = set(city)
-print(city)
+def duplicate_city(cities):
+    result = list()
+    s = set()
+
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2: # 중복 값이 들어온 경우
+            result.append(city)
+
+    return result
+
+
+
+
+
+
+cities =['Incheon','Incheon','Incheon','Gimpo','Seoul','Seoul']
+# cities = {'Incheon','Incheon','Incheon','Gimpo','Seoul','Seoul'}
+cities.append('Anyang')
+cities.append('Seoul')
+print(cities)
+print(set(duplicate_city(cities)))
