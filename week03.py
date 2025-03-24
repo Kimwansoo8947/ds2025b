ec44fcbf2b45cbcb883a30343d2171af2c04b6e9
kimwansoo8947 <ch3558947@naver.com>
2025-03-24 11:45:28 +0900

    07 array

--- week03.py
+++ week03.py
@@ -1,6 +1,11 @@
-groups = ['HOT','Seventeen', 'Black Pink',"NJZ"]
-#ratings = [1,2,4,3,100]
-ratings = [1,2,4,3]
+# groups = ['HOT','Seventeen', 'Black Pink',"NJZ"]
+# #ratings = [1,2,4,3,100]
+# ratings = [1,2,4,3]
+#
+# groups_rating = list(zip(groups, ratings)) # zip 함수
+# print(groups_rating)
 
-groups_rating = list(zip(groups, ratings)) # zip 함수
-print(groups_rating)
+city =['Incheon','Incheon','Incheon','Gimpo','Seoul','Seoul']
+# city = {'Incheon','Incheon','Incheon','Gimpo','Seoul','Seoul'}88
+city = set(city)
+print(city)
