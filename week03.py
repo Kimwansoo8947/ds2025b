56d32d630d86e84e1840e7f355fba1d5c0599b0e
kimwansoo8947 <ch3558947@naver.com>
2025-03-24 11:26:34 +0900

    three_02

--- week03.py
+++ week03.py
@@ -1,7 +1,26 @@
-# 3주차 배열
-import array
+def move_zeros(a_list):
+    zero_index = 0
 
-arr = array.array ('f' ,[11,9,-77,8]) # 파이썬은 배열 개념이 없음
+    for index,  n in enumerate(a_list):
+        if n != 0:
+            a_list[zero_index] = n
+            if zero_index != index:
+                a_list[index] = 0
+            zero_index += 1
 
-for i in range(len(arr)):
-    print(f"{arr[i]:3}{id(arr[i])}")
+    return (a_list)
+a_list = [8,0,3,0,12]
+move_zeros(a_list)
+print(a_list)
+
+
+
+# for i,v in enumerate(l): # unpacking
+   # print(i,v)
+# for i in enumerate(l): # packing
+   #  print(i) # 인덱스랑 값을 동시에 리턴 (튜플)
+
+
+# l = [11,9,-77,8]
+# for i in range(len(l)):
+    # print(i, l[i])
