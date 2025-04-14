79d29af60c8880a72e463489810855ea775ac7fc
kimwansoo8947 <ch3558947@naver.com>
2025-04-14 11:42:20 +0900

    v2.7 queue

--- week06.py
+++ week06.py
@@ -21,11 +21,28 @@ class Queue:
             self.rear.link = node
             self.rear = node
 
+    def deque(self):
+        if self.front is None:
+            raise IndexError("Queue is empty!")
+
+        # 무언가 큐에 데이터가 있다는 뜻
+        self.size = self.size - 1
+        temp = self.front
+        self.front = self.front.link
+
+        if self.front is None:
+            self.rear = None
+
+        return temp.data
+
+
 
 q = Queue()
 q.enqueue("Database")
 q.enqueue("Data structure")
 print(q.size, q.front.data, q.rear.data)
-
+q.deque()
+q.deque()
+print(q.size, q.front, q.rear)
 
 
