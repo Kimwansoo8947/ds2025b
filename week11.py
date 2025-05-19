fc1b73f4aa7fd32308e569c8be2be7d7937837e6
kimwansoo8947 <ch3558947@naver.com>
2025-05-19 12:04:01 +0900

    v4.6 Graph bfs

--- week11.py
+++ week11.py
@@ -17,8 +17,15 @@ def dfs(g, i, visited):
         if g[i][j] == 1 and not visited[j]: # i는 행 j는 열 0을 not 시키면 1 or 1을 not 시키면 0
             dfs(g,j,visited ) # 재귀 함수
 
+
+def bfs(g,i, visited): # 큐 사용
+    pass
+
 visited_dfs = [0 for _ in range(len(graph))]
+visited_bfs = [0 for _ in range(len(graph))]
+
 dfs(graph, 0, visited_dfs) # i는 starting point
+bfs(graph, 6 , visited_bfs)
 
 # v_dfs = [0,0,0,0,0,0,0,0] # A B C D E F G H
 # 방문했던 곳은 return
