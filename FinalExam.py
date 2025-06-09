847e9c03aecdcbe7a8eecddf0c161f59048ae11a
kimwansoo8947 <ch3558947@naver.com>
2025-06-09 14:37:39 +0900

    v5.4 boj 1929

--- FinalExam.py
+++ FinalExam.py
@@ -1,99 +1,21 @@
+import math
+import sys
 
-class Graph:
-    def __init__(self, size):
-        self.graph = [[0 for _ in range(size)] for _ in range(size)]
-
-
-class DisjointSet:
-
-    def __init__(self,n):
-        self.parent = [i for i in range(n)]
-
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x]  = self.find(self.parent[x])
-
-        return self.parent[x]
-
-    def union(self, x, y):
-        x_root = self.find(x)
-        y_root = self.find(y)
-
-        if x_root != y_root:
-            self.parent[y_root] = x_root
-            return True
+def is_prime(n) -> bool:
+    if n<=1: # 1 이하는 소수가 아니다.
         return False
-
-def print_graph(g):
-    print(' ', end= ' ')
-
-    for v in range(len(g.graph)):
-        print(name_ary[v], end = ' ')
-
-    print()
-
-    for row in range(len(g.graph)):
-        print(name_ary[row], end = ' ')
-
-        for col in range(len(g.graph)):
-            print(f"{g.graph[row][col]:2d}", end = ' ')
-
-        print()
-
-    print()
+    elif n == 2: # 2는 소수
+        return True
+    elif n %2 == 0: # 짝수는 2를 제외하고 소수가 아니다.
+        return False
+    else:
+        for i in range(3, int(math.sqrt(n))+1,2): # 홀수만 검사
+            if n % i ==0: # 나누어 떨어지면 소수가 아니다.
+                return False
+        return True # 위 조건을 모두 만족하면 소수
 
 
-
-g1 = None
-name_ary = ['인천', '서울', '강릉', '대전', '광주', '부산']
-Incheon, seoul, gangneung, daejeon, gwangju, busan = 0, 1, 2, 3, 4, 5
-
-
-graph_size = 6
-g1 = Graph(graph_size) # 6행 6열짜리
-g1.graph[Incheon][seoul] = 10; g1.graph[Incheon][gangneung] = 15
-g1.graph[seoul][Incheon] = 10; g1.graph[seoul][gangneung] = 40; g1.graph[seoul][daejeon] = 11; g1.graph[seoul][gwangju] = 55
-g1.graph[gangneung][Incheon] = 15; g1.graph[gangneung][seoul] = 40; g1.graph[gangneung][daejeon] = 12
-g1.graph[daejeon][seoul] = 11; g1.graph[daejeon][gangneung] = 12; g1.graph[daejeon][gwangju] = 20; g1.graph[daejeon][busan] = 30
-g1.graph[gwangju][seoul] = 55; g1.graph[gwangju][daejeon] = 20; g1.graph[gwangju][busan] = 28
-g1.graph[busan][daejeon] = 30; g1.graph[busan][gwangju] = 28
-
-print("도시 간 도로 건설을 위한 전체 연결도")
-print_graph(g1)
-
-# 크루스컬 알고리즘
-edge_ary = []
-
-for i in range(graph_size):
-    for j in range(graph_size):
-        if g1.graph[i][j] != 0:
-            edge_ary.append([g1.graph[i][j],i,j])
-
-print(edge_ary)
-
-# 오름 차순으로 정렬
-edge_ary.sort()
-print(edge_ary)
-
-ds = DisjointSet(graph_size)
-mst_edges = list()
-mst_cost = 0
-
-for cost, s,e in edge_ary:
-    if ds.union(s,e):
-        mst_edges.append((cost, s, e))
-        mst_cost = mst_cost + cost
-
-mst_graph = Graph(graph_size)
-for cost, s,e in mst_edges:
-    mst_graph.graph[s][e] = cost
-    mst_graph.graph[e][s] = cost
-
-print("최소 비용 계산")
-print_graph(mst_graph)
-
-print(f"최소 비용: {mst_cost}")
-
-print('\n MST 간선 목록')
-for cost, u,v in mst_edges:
-    print(f"{name_ary[u]} --- {name_ary[v]} : {cost}")
+s,e = map(int, input().split())
+for i in range(s, e+1):
+    if is_prime(i):
+        print(i)
